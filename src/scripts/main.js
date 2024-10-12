8230a364e78d06173e22b86e1b2fb26f2d031513
an-marryKyslenko <olih.a@icloud.com>
2024-10-12 10:36:23 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const messageDiv = document.createElement('div');
+
+messageDiv.classList.add('message');
+
+function handler() {
+  const promise1 = Promise.resolve('Promise was resolved!');
+
+  promise1.then((result) => {
+    messageDiv.innerText = result;
+    document.body.appendChild(messageDiv);
+  });
+
+  const promise2 = Promise.reject(new Error('Promise was rejected!'));
+
+  promise2.catch((error) => {
+    setTimeout(() => {
+      messageDiv.classList.add('error-message');
+      messageDiv.innerText = error.message;
+    }, 3000);
+  });
+}
+
+logo.addEventListener('click', handler);
